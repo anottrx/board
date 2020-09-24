aafb5653b49882286bfe6da22c4e617ff75b26e2
anottrx <anottrx@gmail.com>
2020-09-24 23:47:54 +0900

    #0 Clock Form
    요일 추가

--- src/LiveClock.js
+++ src/LiveClock.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Clock from "react-live-clock";
+import "moment/locale/ko";
 
 class LiveClock extends Component {
   render() {
     return (
       <div>
-        <Clock format={`YYYY년 MM월 DD일 HH:mm:ss`} ticking={true} />
+        <Clock format={`YYYY년 MM월 DD일 dddd HH:mm:ss`} ticking={true} />
       </div>
     );
   }
