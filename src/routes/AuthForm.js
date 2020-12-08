b472082da1a6373e7077384b36ba3f949a4bdb89
anottrx <anottrx@gmail.com>
2020-12-08 21:45:09 +0900

    #0 Form Fix

--- src/routes/AuthForm.js
+++ src/routes/AuthForm.js
@@ -5,6 +5,9 @@ const AuthForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
+  const [year, setYear] = useState("");
+  const [month, setMonth] = useState("");
+  const [date, setDate] = useState("");
   const [error, setError] = useState("");
 
   const onChange = (event) => {
@@ -17,12 +20,22 @@ const AuthForm = () => {
       setPassword(value);
     } else if (name === "password2") {
       setPassword2(value);
+    } else if (name === "year") {
+      setYear(value);
+    } else if (name === "month") {
+      setMonth(value);
+    } else if (name === "date") {
+      setDate(value);
     }
   };
   const onSubmit = (event) => {
     event.preventDefault();
-    if ({ password } !== { password2 }) {
-      setError("비밀번호가 다릅니다");
+    // if ({ password } !== { password2 }) {
+    //   setError("비밀번호가 다릅니다");
+    // }
+    let num = { year };
+    if (parseInt(num) <= parseInt(1920)) {
+      setError("정말이세요?");
     }
     try {
     } catch (error) {
@@ -61,6 +74,31 @@ const AuthForm = () => {
           type="password"
           required
           value={password2}
+          onChange={onChange}
+        />
+      </label>
+      <br />
+      <label>
+        생년월일
+        <input
+          name="year"
+          type="text"
+          required
+          value={year}
+          onChange={onChange}
+        />
+        <input
+          name="month"
+          type="text"
+          required
+          value={month}
+          onChange={onChange}
+        />
+        <input
+          name="date"
+          type="text"
+          required
+          value={date}
           onChange={onChange}
         />
       </label>
--- src/routes/AuthLogin.js
+++ src/routes/AuthLogin.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { Component, useState } from "react";
 
 // 로그인
-const AuthLogin = () => {
+function AuthLogin() {
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
+  const onLogin = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    console.log(event);
+  };
+
   return (
     <>
       <form className="login">
         <label>
           아이디
-          <input type="email" required />
+          <input value={id} type="email" required />
         </label>
         <br />
         <label>
           비밀번호
-          <input type="password" required />
+          <input value={pw} type="password" required />
         </label>
         <br />
-        <button>로그인</button>
+        <button onClick={onLogin}>로그인</button>
       </form>
     </>
   );
-};
+}
 
 export default AuthLogin;
